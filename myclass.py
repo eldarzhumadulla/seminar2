0b5f1c356270191bc13d7aae02c159ed28d9d90f
Eldar Zhumadillayev <eldar.zhumadulla@gmail.com>
2019-02-09 00:02:24 +0600

    st2

---
+++ myclass.py
@@ -0,0 +1,16 @@
+class MyClass:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def method1(self):
+    print(s.name)
+    print(s.age)
+  def method2(self):
+    self.name = "Eldos"
+    self.age = "26"
+
+s=MyClass("Eldar", 28)
+s.method1()
+s.method2()
+s.method1()
+
